e784002af4f8b1c33f5b717755b4e75a2e3e349e
Ilish jyotish <40943217+Ilix-studio@users.noreply.github.com>
2024-10-31 08:06:48 +0530

    bug fix 3

--- server/config/allowOrigins.js
+++ server/config/allowOrigins.js
@@ -1,6 +1,7 @@
 const allowOrigins = [
   "http://localhost:5173",
   "http://localhost:5173/general-question",
+  "http://localhost:5173/questions-deck",
   "http://localhost:5173/mcq-form",
 ];
 export default allowOrigins;
